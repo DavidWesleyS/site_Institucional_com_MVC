bf4ea033b0dba03d76adff3d87f42f4dce33fc13
David DEV <davidwesleys2003@gmail.com>
2024-08-31 20:21:42 -0300

    atualização

---
+++ app/controllers/HomeControllers.php
@@ -0,0 +1,20 @@
+<?php
+//Carrega as configurações de rotas
+    require 'config/routes.php';
+
+//Função simples para autoload das classes 
+    spl_autoload_register(function($class)) {
+        if (file_exists("../app/controllrs/{$class}.php")) {
+            re
+        }
+
+    }
+
+
+
+
+
+   
+?>
+
+
